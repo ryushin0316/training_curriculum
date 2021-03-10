6989f6e7fa42540207f1cca8b772971c9f713027
ryushin0316 <the.animal.kingdom18@gmail.com>
2021-03-11 00:07:32 +0900

    Issue5

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'posts', to: 'calendars#index'
   post 'calendar', to: 'calendars#create'
+  require "date"
+
+  puts Date.today
 end

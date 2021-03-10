fef162dc6ffaa7ef4d82fc3809172c111fcec7cc
ryushin0316 <the.animal.kingdom18@gmail.com>
2021-03-10 23:59:31 +0900

    Issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -10,6 +10,7 @@ class CalendarsController < ApplicationController
   def create
     Plan.create(plan_params)
     redirect_to action: :index
+    Post.create(content: params[:content])
   end
 
   private
